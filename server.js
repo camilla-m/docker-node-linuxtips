2209d2fdf2f74c9482fab45b0668bca27f02d9d3
Camilla Martins <camidevops@gmail.com>
2021-09-09 02:18:47 -0300

    Get latest image

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Punk do DevOps ECR');
+  res.send('Punk do DevOps GITLAB!');
 });
 
 app.listen(PORT, HOST);
