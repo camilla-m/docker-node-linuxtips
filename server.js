cb28b05536f5d8267630134025f5a469979b4072
Camilla Martins <camidevops@gmail.com>
2021-09-08 21:37:37 -0300

    First commit on project

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Punk do DevOps ECR');
+});
+
+app.listen(PORT, HOST);
