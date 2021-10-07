2a6205a4386232feef25449d715bb62b93201f9b
Camilla Martins <camidevops@gmail.com>
2021-10-06 22:38:04 -0300

    (feature/node): change app get send

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Punk do DevOps GITLAB!');
+  res.send('Punk do DevOps!');
 });
 
 app.get('*', (req, res) => {
